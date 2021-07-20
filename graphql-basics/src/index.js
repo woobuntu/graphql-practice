c32af93b6b807994be5e8847dbbcbbc3cef19415
woobuntu <boutime2017@gmail.com>
2021-07-20 14:58:02 +0900

    graphql 구현체인 graphql-yoga 사용

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,27 @@
-console.log("Hello GraphQL");
+import { GraphQLServer } from "graphql-yoga";
+
+// Type definitions (schema)
+
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my first query!";
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log("The server is up!");
+});
