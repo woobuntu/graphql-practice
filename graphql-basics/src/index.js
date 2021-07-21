2f8736c1101e4785c7b890407dd76a1ce1cd4cf6
woobuntu <boutime2017@gmail.com>
2021-07-21 14:41:12 +0900

    operation arguments

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -4,7 +4,10 @@ import { GraphQLServer } from "graphql-yoga";
 
 const typeDefs = `
     type Query {
+      greeting(name: String, position:String): String!
       me: User!
+      post: Post!
+      add(a: Float!, b: Float!): Float!
     }
 
     type User {
@@ -13,11 +16,24 @@ const typeDefs = `
       email: String!
       age: Int 
     }
+
+    type Post {
+      id: ID!
+      title: String!
+      body: String!
+      published: Boolean!
+    }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
+    greeting(parent, args, ctx, info) {
+      const { name, position } = args;
+      return `Hello${
+        name && position ? `, ${name} You are my favorite ${position}` : ""
+      }!`;
+    },
     me() {
       return {
         id: "1",
@@ -25,6 +41,17 @@ const resolvers = {
         email: "woobuntu@mail.mail",
         age: 30,
       };
+    },
+    post() {
+      return {
+        id: "1",
+        title: "GraphQL",
+        body: "에라이",
+        published: false,
+      };
+    },
+    add(_, { a, b }) {
+      return a + b;
     },
   },
 };
