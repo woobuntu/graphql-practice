fc08a4f7d1750a299498115569e958894d83503a
in uh, lee <boutime2017@gmail.com>
2021-08-04 17:30:25 +0900

    subscription demo

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,12 +1,15 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
 import resolvers from "./resolvers";
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql", // root 디렉토리 기준 상대경로
   resolvers,
   context: {
     db,
+    pubsub, // Subscription resolver에서 필요로 한다.
   },
 });
 
